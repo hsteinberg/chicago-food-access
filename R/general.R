22b83c647a99ad0ebcaeb5ffcc1bab8cc558ee9b
hsteinberg <hsteinb@umich.edu>
2020-06-05 19:48:53 -0400

    sample zip codes

---
+++ R/general.R
@@ -0,0 +1,3 @@
+cook_zipcodes_sample = c(north="60625", west="60804",south="60620",
+                         central="60601",northsub="60026",northwestsub="60004",
+                         westsub="60546",southsub="60426",southwestsub="60465")
