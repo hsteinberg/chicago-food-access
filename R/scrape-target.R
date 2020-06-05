eba2f629e06d92b7fa62565f58aa3be8b060f416
hsteinberg <hsteinb@umich.edu>
2020-06-05 19:49:04 -0400

    init commit target

---
+++ R/scrape-target.R
@@ -0,0 +1,39 @@
+library(tidyverse)
+library(magrittr)
+library(devtools)
+library(RSelenium)
+
+#source functions to make rselenium easier
+source_url("https://github.com/hsteinberg/ccdph-functions/blob/master/general-use-rselenium-functions.R?raw=TRUE")
+
+#source general code
+source("R/general.R")
+
+#start firefox session
+start_server()
+
+get_target_by_zipcode = function(zipcode){
+  #navigate to locator site
+  rD$navigate(paste0("https://www.target.com/store-locator/find-stores/",zipcode))
+  Sys.sleep(2)
+  
+  #Get table data
+  data = rD$findElement(using = "css", "div.Row-uds8za-0:nth-child(2)")$getElementText()[[1]] %>%
+    gsub("\nthird party advertisement|\nsponsored", "", .)
+  
+  stores = strsplit(data, "\n")[[1]]
+  target = as.data.frame(matrix(stores, ncol = 5,  byrow = TRUE), stringsAsFactors = FALSE)[,1:4] %>%
+    set_colnames(c("Name", "Address", "Hours", "Phone")) %>%
+    mutate(Closed = grepl("closed today", Hours),
+           Name = paste("Target", Name)
+    )
+  
+  return(target)
+}
+
+targets = lapply(cook_zipcodes_sample, get_target_by_zipcode) %>%
+  bind_rows() %>%
+  unique()
+
+write_csv(targets, paste0("data/", Sys.Date(), "-targets.csv"))
+
