bc9c1e21763f8930f5bbc8dd2e4e3762ff171e10
hsteinberg <hsteinb@umich.edu>
2020-06-05 20:50:04 -0400

    init commit dollar general

--- R/general.R
+++ R/general.R
@@ -1,3 +1,13 @@
+library(tidyverse)
+library(magrittr)
+library(devtools)
+library(RSelenium)
+
+#source functions to make rselenium easier
+source_url("https://github.com/hsteinberg/ccdph-functions/blob/master/general-use-rselenium-functions.R?raw=TRUE")
+
+#zip codes spread out through the county since most store locator websites only show a certain number of stores,
+#have to search by many zip codes
 cook_zipcodes_sample = c(north="60625", west="60804",south="60620",
                          central="60601",northsub="60026",northwestsub="60004",
                          westsub="60546",southsub="60426",southwestsub="60465")
---
+++ R/scrape-dollar-general.R
@@ -0,0 +1,50 @@
+#source general code
+source("R/general.R")
+
+#start firefox session
+start_server()
+
+#go to Dollar General website
+rD$navigate("https://hosted.where2getit.com/dollargeneral/index.html")
+
+#Get data by zip code search
+get_dollar_gen_by_zip = function(zipcode){
+  
+  #enter zipcode
+  click("#address")
+  enter_text("#address", c(zipcode, key = "enter"))
+  
+  #sleep to wait for load
+  Sys.sleep(2)
+  
+  #get text for all stores
+  stores = rD$findElement(using = "css", "#poiblock")$getElementText()[[1]]
+  stores = gsub("^.*STORES NEAR YOU\n", "", stores)
+  stores = strsplit(stores,"\n\\d\\.\\d\\d miles")[[1]]
+  
+  #get info for each store
+  df = sapply(1:length(stores), function(i){
+    x = stores[i]
+    split = strsplit(x, "\n")[[1]]
+    
+    Name = str_extract(x, "Dollar General # \\d+")
+    Address = paste0(split[5], ", ", split[6])
+    Phone = str_extract(x, "\\(\\d{3}\\) \\d{3}-\\d{4}")
+    Closed = grepl("Temporarily Closed", x)
+    out = c("Name" = Name, "Address" = Address, "Phone" = Phone, "Closed"=Closed)
+    return(out)
+  }) %>%
+    t() %>%
+    as.tibble()
+  
+  return(df)
+  
+}
+
+dollar_gens = lapply(cook_zipcodes_sample, get_dollar_gen_by_zip) %>%
+  bind_rows() %>%
+  unique()
+
+write_csv(dollar_gens, paste0("data/", Sys.Date(), "-dollar-general.csv"))
+
+stop_server()
