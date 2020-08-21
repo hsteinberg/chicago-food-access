4b622c0991c2f5d4d856c205aadbfe1321815841
ldraftz <lauren.e.draftz@gmail.com>
2020-08-20 21:45:58 -0500

    Scraping Savealot Initial commit

---
+++ R/scrape-savealot.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(magrittr)
+library(devtools)
+library(RSelenium)
+
+#source functions to make rselenium easier
+source_url("https://github.com/hsteinberg/ccdph-functions/blob/master/general-use-rselenium-functions.R?raw=TRUE")
+
+#zip codes spread out through the county since most store locator websites only show a certain number of stores,
+#have to search by many zip codes
+cook_zipcodes_sample = c(north="60625", west="60804",south="60620",
+                         central="60601",northsub="60026",northwestsub="60004",
+                         westsub="60546",southsub="60426",southwestsub="60465")
+
+source("R/general.R")
+stop_server()
+start_server()
+rD$navigate("https://savealot.com/save-lot-grocery-stores-near-me")
+scrape_savealot_by_zip= function(zipcode){
+  
+  enter_text("#inputaddress", zipcode)
+  
+ store_info= get_text_class(".sb-location", selectorType="css") 
+ 
+  
+}
