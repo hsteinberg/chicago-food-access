a6d177e4e05d77029f9007540b5f93a8cc141af2
hsteinberg <hsteinb@umich.edu>
2020-09-18 15:22:22 -0400

    daily update

--- R/run-all.R
+++ R/run-all.R
@@ -1,7 +1,7 @@
 rm(list = ls())
 source("R/scrape-dollar-general.R")
-source("R/scrape-target.R")
 source("R/scrape-walgreens.R")
 source("R/scrape-walmart.R")
 source("R/scrape-cvs.R")
+source("R/scrape-target.R")
 stop_server()
